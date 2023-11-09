49ca9cef35a808a159b290d08c2bd003f3e66a2a
Rina-24 <rina.k.game@gmail.com>
2023-11-09 14:36:48 +0900

    11/9 講義

---
+++ code2_6.py
@@ -0,0 +1,4 @@
+# ネットワーク、データベース、セキュリティ試験の点数
+scores = [88,90,95]
+total = sum(scores)
+print(f"合計{total}点")
---
+++ code2_7.py
@@ -0,0 +1,5 @@
+# ネットワーク、データベース、セキュリティ試験の点数
+scores = [88,90,95]
+total = sum(scores)
+avg = total / len(scores)
+print(f"合計{total}点、平均{int(avg)}点")
