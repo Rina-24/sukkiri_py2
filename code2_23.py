07aac99e558f3a65fa5c929d89217da07e0e26d5
Rina-24 <rina.k.game@gmail.com>
2023-11-13 13:30:45 +0900

    教科書P115　リストとセット

--- code2_23.py
+++ code2_23.py
@@ -1,5 +1,5 @@
 scores = {"network":60,"database":80,"security":60}
 members = ["松田","浅木","工藤"]
-print(tuple(members))
-print(list(scores))
-print(set(scores.values()))
+print(tuple(members)) # リストmembersをタプルに変換して表示
+print(list(scores)) # scoresのキーをリストに変換して表示
+print(set(scores.values())) # scoresの値をセットに変換して表示
